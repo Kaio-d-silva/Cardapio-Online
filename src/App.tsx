689457f4a33b3700fd6d72212f68738f0afbea9b
Your Name <you@example.com>
2025-06-25 21:30:40 -0400

    maratona

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from "./pages/Home";
-import DetalhesPrato from "./componetes/DetalhesPrato";
-import CadastrarPrato from "./componetes/CadastrarPrato";
+import Home from './pages/Home';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/detalhes-prato/:id" element={<DetalhesPrato />} />
-        <Route path="/cadastrar-prato" element={<CadastrarPrato />} />
       </Routes>
     </Router>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,56 +1,29 @@
-import "../estilos/Home.css";
-import terraDasAguas from "../assets/terra_das_aguas.jpg";
-import CardPratoComponent from "../componetes/CardPrato";
-import { useState } from "react";
+import "../assets/styles/home.css"
+const image = "./terra_das_aguas.jpg"
 
-function Home() {
-  const [prato, setPrato] = useState([
-    {
-      nome : 'carreteiro',
-      cozinha : 'brasileira',
-      descricao : 'Comida de caminhoneiro',
-      imagem : 'https://catracalivre.com.br/cdn-cgi/image/f=auto,q=60,w=640,h=310,fit=cover/wp-content/uploads/2025/03/arroz-carreteiro-scaled.png'
-    },
-    {
-      nome : 'carreteiro',
-      cozinha : 'brasileira',
-      descricao : 'Comida de caminhoneiro',
-      imagem : 'https://catracalivre.com.br/cdn-cgi/image/f=auto,q=60,w=640,h=310,fit=cover/wp-content/uploads/2025/03/arroz-carreteiro-scaled.png'
-    },
-    {
-      nome : 'carreteiro',
-      cozinha : 'brasileira',
-      descricao : 'Comida de caminhoneiro',
-      imagem : 'https://catracalivre.com.br/cdn-cgi/image/f=auto,q=60,w=640,h=310,fit=cover/wp-content/uploads/2025/03/arroz-carreteiro-scaled.png'
-    },
-    {
-      nome : 'carreteiro',
-      cozinha : 'brasileira',
-      descricao : 'Comida de caminhoneiro',
-      imagem : 'https://catracalivre.com.br/cdn-cgi/image/f=auto,q=60,w=640,h=310,fit=cover/wp-content/uploads/2025/03/arroz-carreteiro-scaled.png'
-    },
-  ]);
-
-  return (
-    <div className="home">
-      <div className="banner">
-        <img src={terraDasAguas} alt="" />
-      </div>
-      <h1>Bem vindo ao Restaurante Terra das Aguas SENAC - MS</h1>
-      <div className="lista-pratos">
-        {prato.map((prato, index) => (
-          <CardPratoComponent 
-          key = {index}
-          nome={prato.nome}
-          cozinha={prato.cozinha}
-          descricao={prato.descricao}
-          imagem={prato.imagem}
-          />
-
-        ))}
-      </div>
-    </div>
-  );
+const Home = () => {
+    return(
+        <>
+        <main className="main-home">
+        <div>
+            <img src={image} alt="sem imagem" />
+        </div>
+        <div>
+            <div className="container-cards"> 
+                <div className="card-prato">
+                    card 1
+                </div>
+                <div className="card-prato">
+                    card 2
+                </div>
+                <div className="card-prato">
+                    card 3
+                </div>
+            </div>
+        </div>
+        </main>
+        </>
+    )
 }
 
-export default Home;
+export default Home 
