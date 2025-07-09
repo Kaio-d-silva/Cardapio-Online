52416e33e03d84c4035cc57c148baa10407dea12
Your Name <you@example.com>
2025-07-08 21:06:53 -0400

    correção

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import DetalhesPrato from './components/DetalhesPrato';
-import CadastrarPrato from './components/CadastrarPrato';
-import EditarPrato from './components/EditarPrato';
 import FormularioPrato from './components/FormularioPrato';
 
 function App() {
@@ -11,8 +9,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/detalhes-prato/:id" element={<DetalhesPrato />} />
-        <Route path="/editar-prato/:id" element={<EditarPrato/>} />
-        <Route path="/cadastrar-prato" element={<CadastrarPrato />} />
         <Route path= "/formulario-prato" element={<FormularioPrato/>}/>
         <Route path= "/formulario-prato/:id" element={<FormularioPrato isEditing={true}/>}/>
       </Routes>
